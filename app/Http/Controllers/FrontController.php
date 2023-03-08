f4f03d53bf593724a25a9cc131c62848b929608c
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-08 23:29:45 +0100

    Create FrontController.php

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    /** Handle the incoming request. */
+    public function __invoke(Request $request)
+    {
+        // return view('front.index');
+        return view('welcome');
+    }
+}
