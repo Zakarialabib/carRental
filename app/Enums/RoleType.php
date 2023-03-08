7fe422e8dc8d3c7e5c392dcec5586a8532c1de36
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-09 00:16:23 +0100

    minor update

--- app/Enums/RoleType.php
+++ app/Enums/RoleType.php
@@ -6,8 +6,6 @@ enum RoleType: string
     case ADMIN = 'admin';
     case CLIENT = 'client';
 
-
-
     public static function values(): array
     {
         return array_column(self::cases(), 'name', 'value');
