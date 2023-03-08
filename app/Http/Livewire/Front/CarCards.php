582081ccb98cab060c9ac8ba3a82903665698a94
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-09 00:13:36 +0100

    Create CarCards.php

---
+++ app/Http/Livewire/Front/CarCards.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire\Front;
+
+use Livewire\Component;
+use App\Models\Car;
+use Livewire\WithPagination;
+
+class CarCards extends Component
+{
+    use WithPagination;
+    public function getCarsProperty()
+    {
+        return Car::select('id','name','model')->paginate(5);
+    }
+    public function render()
+    {
+        return view('livewire.front.car-cards');
+    }
+}
