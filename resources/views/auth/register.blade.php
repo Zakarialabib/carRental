0409a9dcb76d32787b109c7bb9decb0910d4df9a
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-08 23:35:49 +0100

    Update register.blade.php
    with phone & role

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,13 +9,27 @@
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
+        <!-- Phone -->
+        <div class="mt-4">
+            <x-input-label for="phone" :value="__('Phone')" />
+            <x-text-input id="phone" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+        </div>
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <select class="w-full" id="type" name="type">
+                @foreach(App\Enums\RoleType::values() as $key=>$value)
+                <option value="{{ $key }}">{{ $value }}</option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('type')" class="mt-2" />
+        </div>
+        
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
