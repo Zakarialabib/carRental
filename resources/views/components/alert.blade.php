3952f957eb0b78b014e249c830dcea3789252b33
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-13 16:24:51 +0100

    Update alert.blade.php

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,11 +1,11 @@
-<div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-{{ $attributes['variant'] }}-500">
-    <span class="text-xl inline-block mr-5 align-middle">
-        <i class="fas fa-bell"></i>
-    </span>
-    <span class="inline-block align-middle mr-8">
-        {{ $attributes['message'] }}
-    </span>
-    <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none" onclick="closeAlert(event)">
-        <span>×</span>
-    </button>
-</div>
+@if(session()->has('success'))
+    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+        <strong class="font-bold">{{ session('success') }}</strong>
+    </div>
+@endif
+
+@if(session()->has('error'))
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+        <strong class="font-bold">{{ session('error') }}</strong>
+    </div>
+@endif
