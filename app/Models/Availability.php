a7c9c56e45f91f888d9bb78b507705a5a9bb41c7
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-13 17:32:39 +0100

    Availability modeling

---
+++ app/Models/Availability.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Status;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Availability extends Model
+{
+    use HasFactory;
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'price',
+        'start_date',
+        'pickup_time',
+        'end_date',
+        'dropoff_time',
+        'location',
+        'description',
+        'car_id',
+        'user_id',
+        'status',
+    ];
+
+      /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'status' => Status::class,
+    ];
+
+    public function car()
+    {
+        return $this->belongsTo(Car::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+}
